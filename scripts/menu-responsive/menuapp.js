21b18ed8adfd10459af89a9a3d17cefeabcab98a
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2024-01-07 17:14:09 -0300

    add resposivity

---
+++ scripts/menu-responsive/menuapp.js
@@ -0,0 +1,7 @@
+//Active responsive menu
+const buttomMenu = document.querySelector('#botao-menu')
+buttomMenu.addEventListener('click', e => {
+    buttomMenu.classList.toggle('ativo')
+})
+
+console.log('sdfghj')
